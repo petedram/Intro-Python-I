1500e272928476991d80753afa7a3394caad3920
PeteDram <me@petejames.com>
2020-09-08 20:42:52 -0700

    more stretch

--- src/16_prime.py
+++ src/16_prime.py
@@ -4,9 +4,9 @@ num = int(input('Enter a number: '))
 if num > 1:
    for i in range(2,num):
        if (num % i) == 0:
-           print(num,'not prime')
+           print(num,'not prime') # is a whole number
            break
    else:
        print(num,'is prime')
-else:
+else: # < or = 1 is not prime
    print(num,'not prime')
